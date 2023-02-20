298b00379d65593e4cc69baa7e55aa01f1d31809
christopher-t-smith <christophert.smith2016@gmail.com>
2023-02-20 22:22:41 +0000

    Challenge: Shebang, input, print, concatenate, variables

---
+++ lab_input/challenge18.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 3
+"""Alta3 Research | RZFeeser
+    print() - display data to std out"""
+
+def main():
+
+    name = input("What is your name? ")
+    day = input("What day is it today? ")
+
+    print(f"Hello, {name}! Happy {day}!")
+
+main()
