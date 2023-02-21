9e9f6d496a289213c5bebe442cbfbeefc9e9f478
christopher-t-smith <christophert.smith2016@gmail.com>
2023-02-21 21:37:21 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Alta3 Research | RZFeeser
+   List - making selections from complex lists"""
+
+def main():
+
+   # create a list called list1
+   list1 = ["fox", "dog", "cat", "bee", "ant"]
+
+   print(*list1) 
+
+main()
