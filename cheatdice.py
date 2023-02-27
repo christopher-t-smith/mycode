fdc2cddd15e1739de4eb4a10d66df4bf97644e8c
christopher-t-smith <christohpert.smith2016@gmail.com>
2023-02-27 16:50:59 +0000

    CHALLENGE: More Cheats

--- cheatdice.py
+++ cheatdice.py
@@ -19,6 +19,33 @@ class Player:
     def get_dice(self): # returns the dice rolls
         return self.dice
 
+class Cheat_Mulligan(Player):
+    def cheat(self):
+       if sum(self.dice) <= 9:
+           self.dice = []
+           for i in range(3):
+              self.dice.append(random.randint(1,6))
+
+# add an extra dice roll
+class Cheat_Extra_Die(Player):
+    def cheat(self):
+        self.dice.append(random.randint(1,6))
+
+# first die roll is lucky and can't roll under a 3
+class Cheat_Lucky_Die(Player):
+    def cheat(self):
+        if self.dice[0] < 3:
+           self.dice[0]= random.randint(3,6)
+
+# switch other player's dice with dice that can't roll above a 3
+class Cheat_Saboteur(Player):
+  def cheat(self, other_player):
+    other_player.dice = [random.randint(1,3) for i in range(3)]
+                        # ^ this is a list comprehension;
+                        # a handy way to generate list contents
+                        # in one line of code
+
+
 # allows user to turn their last roll into a 6
 class Cheat_Swapper(Player):  # inheritance of Player
     def cheat(self):
